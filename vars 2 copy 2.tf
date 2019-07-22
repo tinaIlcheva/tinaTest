ac498c7be4f894a7d2d3df13f11422783d4d439b
Tsvetina Ilcheva <tsvetina.ilcheva@sophos.com>
2019-07-22 12:18:40 +0100

    Create vars 2 copy 2.tf

---
+++ vars 2 copy 2.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {}
+
+variable "customer_id" {}
+
+variable "iam_for_lambda_arn" {}
+
+variable "aws_account_id" {}
+
+variable "iam_for_vpc_flow_arn" {}
+
+variable "should_run" {}
